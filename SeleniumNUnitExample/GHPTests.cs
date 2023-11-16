8510d7ce3ba9cbb4cfd9ad925a3c37784cd758ca
mariabaugustine <maria.baugustine@ust.com>
2023-11-16 13:27:26 +0530

    16/11/2023

---
+++ SeleniumNUnitExample/GHPTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNUnitExample
+{
+    [TestFixture]
+    internal class GHPTests:CoreCodes
+    {
+        [Test]
+        public void TitleTest()
+        {
+            Thread.Sleep(3000);
+            Assert.AreEqual("Google", driver.Title);
+            Console.WriteLine("Title test - Pass");
+        }
+    }
+}
