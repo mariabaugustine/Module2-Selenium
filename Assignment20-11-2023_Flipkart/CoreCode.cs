60a1f8af0924ce8e15f243e1157db7cd9a267d0b
mariabaugustine <maria.baugustine@ust.com>
2023-11-20 16:28:15 +0530

    assignment added

---
+++ Assignment20-11-2023_Flipkart/CoreCode.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment20_11_2023_Flipkart
+{
+    internal class CoreCode
+    {
+        public IWebDriver driver;
+        public Dictionary<string, string> Properties;
+        public void ReadConfigurationFile()
+        {
+            string currentDirectory = Directory.GetParent(@"../../../").FullName;
+            Properties= new Dictionary<string, string>();
+            string path = currentDirectory + "/configsettings/config.properties";
+            string[] file=File.ReadAllLines(path);
+            foreach (string line in file) 
+            {
+                if(!string.IsNullOrWhiteSpace(line)&&line.Contains("="))
+                {
+                    string[] parts = line.Split('=');
+                    string key = parts[0];
+                    string value = parts[1];
+                    Properties[key] = value;
+                }
+            }
+
+        }
+        [OneTimeSetUp]
+        public void InitializeBrowser()
+        {
+            ReadConfigurationFile();
+            if (Properties["browser"].ToLower()=="chrome")
+            {
+                driver=new ChromeDriver();
+            }
+            driver.Url = Properties["baseUrl"];
+            driver.Manage().Window.Maximize();
+        }
+        [OneTimeTearDown] 
+        public void Exit()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ Assignment20-11-2023_Flipkart/FlipkartTests.cs
@@ -0,0 +1,8 @@
+namespace Assignment20_11_2023_Flipkart
+{
+    public class FlipkartTests
+    {
+        [Test]
+
+    }
+}
