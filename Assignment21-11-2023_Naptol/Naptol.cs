4e65fa999a0018d588ff5f09baeca3786c528dc1
mariabaugustine <maria.baugustine@ust.com>
2023-11-21 18:58:48 +0530

    modified

--- Assignment21-11-2023_Naptol/Naptol.cs
+++ Assignment21-11-2023_Naptol/Naptol.cs
@@ -24,18 +24,48 @@ namespace Assignment21_11_2023_Naptol
         }
         [Test]
         [Order(1)]
-        public void SelectFifthProduct()
+        [TestCase(5)]
+        public void SelectFifthProduct(int pid)
         {
-            IWebElement fifthelement = driver.FindElement(By.XPath("/html/body/article/article/section/div[5]/div[1]/div[5]"));
+            DefaultWait<IWebDriver> fwait = new DefaultWait<IWebDriver>(driver);
+            fwait.Timeout = TimeSpan.FromSeconds(5);
+            fwait.PollingInterval = TimeSpan.FromMilliseconds(5);
+            fwait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fwait.Message = "Element not found";
+            string path = "//div[@id='productItem" + pid + "']";
+            IWebElement fifthelement = fwait.Until(d => d.FindElement(By.XPath(path)));
             Actions actions = new Actions(driver);
             Action moveto = () => actions.MoveToElement(fifthelement).Click().Build().Perform();
 
             moveto.Invoke();
+            Thread.Sleep(6000);
 
             List<string> nextTab = driver.WindowHandles.ToList();
             driver.SwitchTo().Window(nextTab[1]);
             
             
         }
+        [Test]
+        [Order(3)]
+        //[TestCase("Black",2.50)]
+        public void AddProductToCart()
+        {
+            DefaultWait<IWebDriver> fwait = new DefaultWait<IWebDriver>(driver);
+            fwait.Timeout = TimeSpan.FromSeconds(5);
+            fwait.PollingInterval = TimeSpan.FromMilliseconds(5);
+            fwait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fwait.Message = "Element not found";
+            //string path = color + "-" + size;
+            IWebElement selectsize = fwait.Until(d => d.FindElement(By.XPath("//a[text()='Black-2.50']")));
+            
+            selectsize.Click();
+            IWebElement submitbutton = fwait.Until(d => d.FindElement(By.XPath("//*[@id=\"cart-panel-button-0\"]/span")));
+            submitbutton.Click();
+            IWebElement status = fwait.Until(d => d.FindElement(By.XPath("//*[text()='My Shopping Cart: ']")));
+            Assert.That(status.Text.Contains("cart"));
+
+
+        }
+
     }
 }
